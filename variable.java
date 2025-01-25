29372d821a8030fe13e289e3e78977b57557004a
KaviyavarshiniSM <kaviyasm22@gmail.com>
2025-01-25 15:33:55 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a=10;
+        String str= "Jesus";
+        float f= 10;
+        double d=4;
+        double data=25112004.8;
+        long s=828630161;
+        short j=8990;
+        System.out.println(a);
+        System.out.println("Value : " +a);
+        System.out.println(str);
+        System.out.println("String : " +str);
+        System.out.println(f);
+        System.out.println("Value: "+f);
+        System.out.println(d);
+        System.out.println(data);
+        System.out.println(s);
+        System.out.println(j);
+    }
+}
